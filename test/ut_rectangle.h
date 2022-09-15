7799b76c6328e959d1f91d7d21c5cee95348cc05
Xanonymous <trusaidlin@gmail.com>
2022-09-16 05:51:34 +0800

    fix: rectangle test should use correct point

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -2,14 +2,13 @@
 
 class RectangleTest : public ::testing::Test {
 protected:
-    RectangleTest() : vec1{TwoDimensionalVector(&_a, &_b)}, vec2{TwoDimensionalVector(&_b, &_c)} {}
+    RectangleTest() : vec1{TwoDimensionalVector(&_a, &_b)}, vec2{TwoDimensionalVector(&_a, &_c)} {}
     const double DEVIATION = 0.0001;
     const TwoDimensionalVector vec1;
     const TwoDimensionalVector vec2;
-    const Point _a{-5, 9};
-    const Point _b{3, 4};
-    const Point _c{3, 9};
-    const Point _d{-5, 4};
+    const Point _a{0, 0};
+    const Point _b{4, 0};
+    const Point _c{0, 3};
 };
 
 TEST_F(RectangleTest, ShouldCorrectlySetLengthAndWidth) {
@@ -17,8 +16,8 @@ TEST_F(RectangleTest, ShouldCorrectlySetLengthAndWidth) {
     const Rectangle rectangle{&vec1, &vec2};
 
     // Act
-    ASSERT_EQ(rectangle.length(), 9.433981132056603);
-    ASSERT_EQ(rectangle.width(), 5);
+    ASSERT_EQ(rectangle.length(), 4);
+    ASSERT_EQ(rectangle.width(), 3);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyCalculateArea) {
@@ -26,7 +25,7 @@ TEST_F(RectangleTest, ShouldCorrectlyCalculateArea) {
     const Rectangle rectangle{&vec1, &vec2};
 
     // Act
-    ASSERT_NEAR(rectangle.area(), 47.169905660283014, DEVIATION);
+    ASSERT_NEAR(rectangle.area(), 12, DEVIATION);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyCalculatePerimeter) {
@@ -34,7 +33,7 @@ TEST_F(RectangleTest, ShouldCorrectlyCalculatePerimeter) {
     const Rectangle rectangle{&vec1, &vec2};
 
     // Act
-    ASSERT_NEAR(rectangle.perimeter(), 28.867962264113206, DEVIATION);
+    ASSERT_NEAR(rectangle.perimeter(), 14, DEVIATION);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyPrintInfo) {
@@ -42,5 +41,5 @@ TEST_F(RectangleTest, ShouldCorrectlyPrintInfo) {
     const Rectangle rectangle{&vec1, &vec2};
 
     // Act
-    ASSERT_EQ(rectangle.info(), "Rectangle (Vector ((-5.00, 9.00), (3.00, 4.00)), Vector ((3.00, 4.00), (3.00, 9.00)))");
+    ASSERT_EQ(rectangle.info(), "Rectangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
 }
