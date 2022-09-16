1ad5a052e165ab90d87ec158d6dc1e39815a3e54
Xanonymous <trusaidlin@gmail.com>
2022-09-17 02:25:12 +0800

    test: add more tests for illegal cases

--- test/ut_triangle.h
+++ test/ut_triangle.h
@@ -34,3 +34,17 @@ TEST_F(TriangleTest, ShouldCorrectlyPrintInfo) {
     // Act
     ASSERT_EQ(triangle.info(), "Triangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
 }
+
+TEST_F(TriangleTest, IllegalTriangle) {
+    // Arrange
+    const Point o{0, 0}, o_1{3, 0}, o_2{-3, 0};
+    const Point k{1, 1}, k_1{4, 1}, k_2{-4, 1};
+    const Point l{0, 2}, l_1{5, 2}, l_2{-5, 2};
+
+    const TwoDimensionalVector vec1{&o, &o_1}, vec2{&o, &o_2}, vec3{&k, &k_1}, vec4{&k, &k_2}, vec5{&k_1, &k_2}, vec6{&o, &l};
+
+    // Act
+    ASSERT_ANY_THROW(Triangle(&vec1, &vec2));
+    ASSERT_ANY_THROW(Triangle(&vec3, &vec1));
+    ASSERT_ANY_THROW(Triangle(&vec5, &vec6));
+}
