dfa9608b3db420090c74fd1257fac5a0242389f2
Xanonymous <trusaidlin@gmail.com>
2022-10-04 04:26:02 +0800

    refactor: refactor circle tests, move circle into test class declare

--- test/ut_circle.h
+++ test/ut_circle.h
@@ -1,42 +1,54 @@
 #include "../src/circle.h"
 
 class CircleTest : public ::testing::Test {
-protected:
-    CircleTest() : vec{TwoDimensionalVector(&_a, &_b)} {}
-    const double DEVIATION = 0.0001;
-    const TwoDimensionalVector vec;
+private:
     const Point _a{-5, 9};
     const Point _b{3, 4};
+    const TwoDimensionalVector vec_{&_a, &_b};
+
+protected:
+    CircleTest() : circle_{&vec_} {}
+
+    const double DEVIATION = 0.0001;
+    const Circle circle_;
 };
 
 TEST_F(CircleTest, ShouldCorrectlySetRadius) {
-    // Arrange
-    const Circle circle{&vec};
-
     // Expect
-    EXPECT_EQ(circle.radius(), 9.433981132056603);
+    EXPECT_EQ(circle_.radius(), 9.433981132056603);
 }
 
 TEST_F(CircleTest, ShouldCorrectlyCalculateArea) {
-    // Arrange
-    const Circle circle{&vec};
-
     // Expect
-    EXPECT_NEAR(circle.area(), 279.60174616949155, DEVIATION);
+    EXPECT_NEAR(circle_.area(), 279.60174616949155, DEVIATION);
 }
 
 TEST_F(CircleTest, ShouldCorrectlyCalculatePerimeter) {
-    // Arrange
-    const Circle circle{&vec};
-
     // Expect
-    EXPECT_NEAR(circle.perimeter(), 59.275451637147491, DEVIATION);
+    EXPECT_NEAR(circle_.perimeter(), 59.275451637147491, DEVIATION);
 }
 
 TEST_F(CircleTest, ShouldCorrectlyPrintInfo) {
-    // Arrange
-    const Circle circle{&vec};
+    // Expect
+    EXPECT_EQ(circle_.info(), "Circle (Vector ((-5.00, 9.00), (3.00, 4.00)))");
+}
+
+TEST_F(CircleTest, ShouldCreateNullIteratorWhenCallCreateBfsIterator) {
+    // Act
+    const auto it = circle_.createBFSIterator();
 
     // Expect
-    EXPECT_EQ(circle.info(), "Circle (Vector ((-5.00, 9.00), (3.00, 4.00)))");
+    EXPECT_TRUE(it->isDone());
+
+    delete it;
+}
+
+TEST_F(CircleTest, ShouldCreateNullIteratorWhenCallCreateDfsIterator) {
+    // Act
+    const auto it = circle_.createDFSIterator();
+
+    // Expect
+    EXPECT_TRUE(it->isDone());
+
+    delete it;
 }
