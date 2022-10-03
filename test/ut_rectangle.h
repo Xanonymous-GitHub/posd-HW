b46344e6aa7dac391aae4d6ca8fd677b0476dabd
Xanonymous <trusaidlin@gmail.com>
2022-10-04 04:30:00 +0800

    refactor: refactor rectangle tests, move rectangle into test class declare

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -1,47 +1,38 @@
 #include "../src/rectangle.h"
 
 class RectangleTest : public ::testing::Test {
-protected:
-    RectangleTest() : vec1{TwoDimensionalVector(&_a, &_b)}, vec2{TwoDimensionalVector(&_a, &_c)} {}
-    const double DEVIATION = 0.0001;
-    const TwoDimensionalVector vec1;
-    const TwoDimensionalVector vec2;
+private:
     const Point _a{0, 0};
     const Point _b{4, 0};
     const Point _c{0, 3};
+    const TwoDimensionalVector vec1{&_a, &_b};
+    const TwoDimensionalVector vec2{&_a, &_c};
+
+protected:
+    RectangleTest() : rectangle_{&vec1, &vec2} {}
+    const double DEVIATION = 0.0001;
+    const Rectangle rectangle_;
 };
 
 TEST_F(RectangleTest, ShouldCorrectlySetLengthAndWidth) {
-    // Arrange
-    const Rectangle rectangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_EQ(rectangle.length(), 4);
-    EXPECT_EQ(rectangle.width(), 3);
+    EXPECT_EQ(rectangle_.length(), 4);
+    EXPECT_EQ(rectangle_.width(), 3);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyCalculateArea) {
-    // Arrange
-    const Rectangle rectangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_NEAR(rectangle.area(), 12, DEVIATION);
+    EXPECT_NEAR(rectangle_.area(), 12, DEVIATION);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyCalculatePerimeter) {
-    // Arrange
-    const Rectangle rectangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_NEAR(rectangle.perimeter(), 14, DEVIATION);
+    EXPECT_NEAR(rectangle_.perimeter(), 14, DEVIATION);
 }
 
 TEST_F(RectangleTest, ShouldCorrectlyPrintInfo) {
-    // Arrange
-    const Rectangle rectangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_EQ(rectangle.info(), "Rectangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
+    EXPECT_EQ(rectangle_.info(), "Rectangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
 }
 
 TEST_F(RectangleTest, IllegalRectangle) {
