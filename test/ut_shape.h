912dcd641f137c7447d741176c4a5ef70a16d021
Xanonymous <trusaidlin@gmail.com>
2022-10-04 04:42:50 +0800

    test: refactored & add rectangle, compound shapes into Shape test

--- test/ut_shape.h
+++ test/ut_shape.h
@@ -6,42 +6,53 @@
 #include "../src/two_dimensional_vector.h"
 
 class ShapeTest : public ::testing::Test {
-protected:
-    ShapeTest() : vec1{TwoDimensionalVector(&_a, &_b)}, vec2{TwoDimensionalVector(&_a, &_c)} {}
-    const double DEVIATION = 0.0001;
-    const TwoDimensionalVector vec1;
-    const TwoDimensionalVector vec2;
+private:
     const Point _a{0, 0};
     const Point _b{4, 0};
     const Point _c{0, 3};
+    const TwoDimensionalVector vec1_{&_a, &_b};
+    const TwoDimensionalVector vec2_{&_a, &_c};
+
+protected:
+    ShapeTest() : circle_{new Circle{&vec1_}},
+                  triangle_{new Triangle{&vec1_, &vec2_}},
+                  rectangle_{new Rectangle{&vec1_, &vec2_}},
+                  compound_{new CompoundShape{*circle_, *triangle_, *rectangle_}} {}
+
+    ~ShapeTest() {
+        delete circle_;
+        delete triangle_;
+        delete rectangle_;
+        delete compound_;
+    }
+
+    const double DEVIATION = 0.0001;
+    const Shape *circle_;
+    const Shape *triangle_;
+    const Shape *rectangle_;
+    const Shape *compound_;
 };
 
 TEST_F(ShapeTest, AreaWithPolymorphismShouldBeCorrect) {
-    // Arrange
-    const Shape *circle = new Circle{&vec1};
-    const Shape *triangle = new Triangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_NEAR(circle->area(), 50.26548245743669, DEVIATION);
-    EXPECT_NEAR(triangle->area(), 6, DEVIATION);
+    EXPECT_NEAR(circle_->area(), 50.26548245743669, DEVIATION);
+    EXPECT_NEAR(triangle_->area(), 6, DEVIATION);
+    EXPECT_NEAR(rectangle_->area(), 12, DEVIATION);
+    EXPECT_NEAR(compound_->area(), 68.26548245743669, DEVIATION);
 }
 
 TEST_F(ShapeTest, PerimeterWithPolymorphismShouldBeCorrect) {
-    // Arrange
-    const Shape *circle = new Circle{&vec1};
-    const Shape *triangle = new Triangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_NEAR(circle->perimeter(), 25.132741228718345, DEVIATION);
-    EXPECT_NEAR(triangle->perimeter(), 12, DEVIATION);
+    EXPECT_NEAR(circle_->perimeter(), 25.132741228718345, DEVIATION);
+    EXPECT_NEAR(triangle_->perimeter(), 12, DEVIATION);
+    EXPECT_NEAR(rectangle_->perimeter(), 14, DEVIATION);
+    EXPECT_NEAR(compound_->perimeter(), 51.132741228718345, DEVIATION);
 }
 
 TEST_F(ShapeTest, InfoWithPolymorphismShouldBeCorrect) {
-    // Arrange
-    const Shape *circle = new Circle{&vec1};
-    const Shape *triangle = new Triangle{&vec1, &vec2};
-
     // Expect
-    EXPECT_EQ(circle->info(), "Circle (Vector ((0.00, 0.00), (4.00, 0.00)))");
-    EXPECT_EQ(triangle->info(), "Triangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
+    EXPECT_EQ(circle_->info(), "Circle (Vector ((0.00, 0.00), (4.00, 0.00)))");
+    EXPECT_EQ(triangle_->info(), "Triangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
+    EXPECT_EQ(rectangle_->info(), "Rectangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00)))");
+    EXPECT_EQ(compound_->info(), "CompoundShape (Circle (Vector ((0.00, 0.00), (4.00, 0.00))), Triangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00))), Rectangle (Vector ((0.00, 0.00), (4.00, 0.00)), Vector ((0.00, 0.00), (0.00, 3.00))))");
 }
