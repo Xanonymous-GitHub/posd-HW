339b4589a6be272504363eac193cbdce763f1571
Xanonymous <trusaidlin@gmail.com>
2022-10-04 04:06:57 +0800

    feat: add error msg to null iterator

--- src/iterator/null_iterator.h
+++ src/iterator/null_iterator.h
@@ -4,14 +4,16 @@
 
 class NullIterator : public Iterator {
 public:
-    void first() const override {}
-
-    const Shape *currentItem() const override {
-        return nullptr;
+    void first() override {
+        throw MethodShouldNotBeRunError{"first() should not be run"};
     }
 
-    void next() const override {
-        throw MethodShouldNotBeRunError{""};
+    const Shape *currentItem() const override {
+        throw MethodShouldNotBeRunError{"currentItem() should not be run"};
+    }
+
+    void next() override {
+        throw MethodShouldNotBeRunError{"next() should not be run"};
     }
 
     bool isDone() const override {
