8107dd576a2110233ec4e2bafa73ef33d97c6ec8
Xanonymous <trusaidlin@gmail.com>
2022-10-04 04:07:15 +0800

    test: add null iterator test

--- test/iterator/ut_null_iterator.h
+++ test/iterator/ut_null_iterator.h
@@ -1,1 +1,22 @@
 #include "../../src/iterator/null_iterator.h"
+
+class NullIteratorTest : public ::testing::Test {
+protected:
+    NullIterator null_it_{};
+};
+
+TEST_F(NullIteratorTest, ShouldThrowWhenFirstIsCalled) {
+    EXPECT_THROW({ null_it_.first(); }, Iterator::MethodShouldNotBeRunError);
+}
+
+TEST_F(NullIteratorTest, ShouldThrowWhenNextIsCalled) {
+    EXPECT_THROW({ null_it_.next(); }, Iterator::MethodShouldNotBeRunError);
+}
+
+TEST_F(NullIteratorTest, ShouldThrowWhenCurrentItemIsCalled) {
+    EXPECT_THROW({ null_it_.currentItem(); }, Iterator::MethodShouldNotBeRunError);
+}
+
+TEST_F(NullIteratorTest, ShouldAlwaysBeDone) {
+    EXPECT_TRUE(null_it_.isDone());
+}
