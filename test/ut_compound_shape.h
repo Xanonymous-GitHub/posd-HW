93a29773205a8d7c4e0524e61a765407aceb8380
Xanonymous <trusaidlin@gmail.com>
2022-10-09 22:56:34 +0800

    test: add addShape & deleteShape tests for compoundShape

--- test/ut_compound_shape.h
+++ test/ut_compound_shape.h
@@ -141,3 +141,390 @@ TEST_F(CompoundShapeTestWithoutNesting, InfoShouldBeCorrect) {
     // Expect
     EXPECT_EQ(actual_info, expected_info);
 }
+
+TEST_F(CompoundShapeTestWithoutNesting, ShouldCorrectlyAddShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    compound_shape_.addShape(&circle_);
+    const std::string actual_info = compound_shape_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithoutNesting, ShouldCorrectlyDeleteShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    compound_shape_.deleteShape(&circle_);
+    const std::string actual_info = compound_shape_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithNesting, AreaShouldBeCorrect) {
+    // Arrange
+    const double area_of_lv1_circle = 4 * 4 * M_PI;
+    const double area_of_lv1_rectangle = 3 * 5;
+    const double area_of_lv1_triangle = 4 * 3 / 2;
+    const double area_of_lv1_compound_shape = (4 * 4 * M_PI) + (3 * 5) + (4 * 3 / 2);
+    const double expected_area = area_of_lv1_circle + area_of_lv1_rectangle + area_of_lv1_triangle + area_of_lv1_compound_shape;
+
+    // Act
+    const double actual_area = compound_shape_n1_.area();
+
+    // Expect
+    EXPECT_NEAR(actual_area, expected_area, DEVIATION);
+}
+
+TEST_F(CompoundShapeTestWithNesting, PerimeterShouldBeCorrect) {
+    // Arrange
+    const double perimeter_of_lv1_circle = 2 * 4 * M_PI;
+    const double perimeter_of_lv1_rectangle = 2 * (3 + 5);
+    const double perimeter_of_lv1_triangle = 4 + 3 + 5;
+    const double perimeter_of_lv1_compound_shape = (2 * 4 * M_PI) + (2 * (3 + 5)) + (4 + 3 + 5);
+    const double expected_perimeter = perimeter_of_lv1_circle + perimeter_of_lv1_rectangle + perimeter_of_lv1_triangle + perimeter_of_lv1_compound_shape;
+
+    // Act
+    const double actual_perimeter = compound_shape_n1_.perimeter();
+
+    // Expect
+    EXPECT_NEAR(actual_perimeter, expected_perimeter, DEVIATION);
+}
+
+TEST_F(CompoundShapeTestWithNesting, InfoShouldBeCorrect) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "CompoundShape ("
+                "Circle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00))"
+                "), "
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "Triangle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 3.00))"
+                ")"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    const std::string actual_info = compound_shape_n1_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithNesting, ShouldCorrectlyAddShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "CompoundShape ("
+                "Circle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00))"
+                "), "
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "Triangle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 3.00))"
+                ")"
+            "), "
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    compound_shape_n1_.addShape(&circle_);
+    const std::string actual_info = compound_shape_n1_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithNesting, ShouldCorrectlyDeleteShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "CompoundShape ("
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "Triangle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 3.00))"
+                ")"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    compound_shape_n1_.deleteShape(&circle_);
+    const std::string actual_info = compound_shape_n1_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithMultipleNesting, AreaShouldBeCorrect) {
+    // Arrange
+    const double area_of_lv1_circle = M_PI * 4 * 4;
+    const double area_of_lv1_rectangle = 3 * 5;
+    const double area_of_lv1_triangle = 0.5 * 4 * 3;
+    const double area_of_lv2_circle = M_PI * 4 * 4;
+    const double area_of_lv2_rectangle = 3 * 5;
+    const double area_of_lv2_triangle = 0.5 * 4 * 3;
+    const double area_of_lv2_compound_shape = (M_PI * 4 * 4) + (3 * 5) + (0.5 * 4 * 3);
+    const double area_of_lv1_compound_shape = area_of_lv2_circle + area_of_lv2_rectangle + area_of_lv2_triangle + area_of_lv2_compound_shape;
+    const double expected_area = area_of_lv1_circle + area_of_lv1_rectangle + area_of_lv1_triangle + area_of_lv1_compound_shape;
+
+    // Act
+    const double actual_area = compound_shape_n2_.area();
+
+    // Expect
+    EXPECT_NEAR(actual_area, expected_area, DEVIATION);
+}
+
+TEST_F(CompoundShapeTestWithMultipleNesting, PerimeterShouldBeCorrect) {
+    // Arrange
+    const double perimeter_of_lv1_circle = 2 * M_PI * 4;
+    const double perimeter_of_lv1_rectangle = 2 * (3 + 5);
+    const double perimeter_of_lv1_triangle = 4 + 3 + sqrt(4 * 4 + 3 * 3);
+    const double perimeter_of_lv2_circle = 2 * M_PI * 4;
+    const double perimeter_of_lv2_rectangle = 2 * (3 + 5);
+    const double perimeter_of_lv2_triangle = 4 + 3 + sqrt(4 * 4 + 3 * 3);
+    const double perimeter_of_lv2_compound_shape = 2 * M_PI * 4 + 2 * (3 + 5) + 4 + 3 + sqrt(4 * 4 + 3 * 3);
+    const double perimeter_of_lv1_compound_shape = perimeter_of_lv2_circle + perimeter_of_lv2_rectangle + perimeter_of_lv2_triangle + perimeter_of_lv2_compound_shape;
+    const double expected_perimeter = perimeter_of_lv1_circle + perimeter_of_lv1_rectangle + perimeter_of_lv1_triangle + perimeter_of_lv1_compound_shape;
+
+    // Act
+    const double actual_perimeter = compound_shape_n2_.perimeter();
+
+    // Expect
+    EXPECT_NEAR(actual_perimeter, expected_perimeter, DEVIATION);
+}
+
+TEST_F(CompoundShapeTestWithMultipleNesting, InfoShouldBeCorrect) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "CompoundShape ("
+                "Circle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00))"
+                "), "
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "Triangle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 3.00))"
+                "), "
+                "CompoundShape ("
+                    "Circle ("
+                        "Vector ((0.00, 0.00), (4.00, 0.00))"
+                    "), "
+                    "Rectangle ("
+                        "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                        "Vector ((0.00, 0.00), (0.00, 5.00))"
+                    "), "
+                    "Triangle ("
+                        "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                        "Vector ((0.00, 0.00), (0.00, 3.00))"
+                    ")"
+                ")"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    const std::string actual_info = compound_shape_n2_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithMultipleNesting, ShouldCorrectlyAddShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "Triangle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 3.00))"
+            "), "
+            "CompoundShape ("
+                "Circle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00))"
+                "), "
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "Triangle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 3.00))"
+                "), "
+                "CompoundShape ("
+                    "Circle ("
+                        "Vector ((0.00, 0.00), (4.00, 0.00))"
+                    "), "
+                    "Rectangle ("
+                        "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                        "Vector ((0.00, 0.00), (0.00, 5.00))"
+                    "), "
+                    "Triangle ("
+                        "Vector ((0.00, 0.00), (4.00, 0.00)), "
+                        "Vector ((0.00, 0.00), (0.00, 3.00))"
+                    ")"
+                ")"
+            "), "
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            ")"
+        ")";
+    // clang-format
+
+    // Act
+    compound_shape_n2_.addShape(&circle_);
+    const std::string actual_info = compound_shape_n2_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
+
+TEST_F(CompoundShapeTestWithMultipleNesting, ShouldCorrectlyDeleteShape) {
+    // Arrange
+    const std::string expected_info =
+        // clang-format off
+        "CompoundShape ("
+            "Circle ("
+                "Vector ((0.00, 0.00), (4.00, 0.00))"
+            "), "
+            "Rectangle ("
+                "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                "Vector ((0.00, 0.00), (0.00, 5.00))"
+            "), "
+            "CompoundShape ("
+                "Circle ("
+                    "Vector ((0.00, 0.00), (4.00, 0.00))"
+                "), "
+                "Rectangle ("
+                    "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                    "Vector ((0.00, 0.00), (0.00, 5.00))"
+                "), "
+                "CompoundShape ("
+                    "Circle ("
+                        "Vector ((0.00, 0.00), (4.00, 0.00))"
+                    "), "
+                    "Rectangle ("
+                        "Vector ((0.00, 0.00), (3.00, 0.00)), "
+                        "Vector ((0.00, 0.00), (0.00, 5.00))"
+                    ")"
+                ")"
+            ")"
+        ")";
+    // clang-format on
+
+    // Act
+    compound_shape_n2_.deleteShape(&triangle_);
+    const std::string actual_info = compound_shape_n2_.info();
+
+    // Expect
+    EXPECT_EQ(actual_info, expected_info);
+}
