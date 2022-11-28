83647b60ad5e6fe73b2359678a8b3dd4e1a8605f
Xanonymous <trusaidlin@gmail.com>
2022-11-29 00:09:18 +0800

    feat: add parser, unsafe

--- src/builder/shape_parser.h
+++ src/builder/shape_parser.h
@@ -8,15 +8,54 @@
 
 class ShapeParser {
 private:
-    Scanner *_scanner;
-    ShapeBuilder *_builder;
+    Scanner scanner_;
+    ShapeBuilder builder_{};
+
+    void parseCircle_() {
+        auto center = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        auto toRadius = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        builder_.buildCircle(center, toRadius);
+    }
+
+    void parseTriangle_() {
+        auto commonPoint = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        auto v1Point = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        auto v2Point = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        builder_.buildTriangle(commonPoint, v1Point, v2Point);
+    }
+
+    void parseRectangle_() {
+        auto commonPoint = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        auto v1Point = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        auto v2Point = new Point{scanner_.nextDouble(), scanner_.nextDouble()};
+        builder_.buildRectangle(commonPoint, v1Point, v2Point);
+    }
 
 public:
-    ShapeParser(std::string input) {}
+    ShapeParser(const std::string &input) : scanner_{input} {}
 
     ~ShapeParser() {}
 
-    void parse() {}
+    void parse() {
+        while (!scanner_.isDone()) {
+            const auto token = scanner_.next();
 
-    std::vector<Shape *> getResult() {}
+            if (token == "Circle") {
+                parseCircle_();
+            } else if (token == "Triangle") {
+                parseTriangle_();
+            } else if (token == "Rectangle") {
+                parseRectangle_();
+            } else if (token == "CompoundShape") {
+                builder_.buildCompoundShape();
+                scanner_.next(); // skip '('
+            } else if (token == ")") {
+                builder_.buildCompoundEnd();
+            }
+        }
+    }
+
+    std::vector<Shape *> getResult() {
+        return builder_.getResult();
+    }
 };
