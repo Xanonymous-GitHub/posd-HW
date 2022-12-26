5ce2b6c247f50ab9736b2a560885bc57143b2751
POSD <lab1321@gmail.com>
2022-12-26 13:56:59 +0800

    exam: subject tests

--- test/graphics/observer/ut_subject.h
+++ test/graphics/observer/ut_subject.h
@@ -1,5 +1,36 @@
 #include "../../../src/graphics/observer/subject.h"
+#include "./mock_observer.h"
 
 class SubjectTest : public ::testing::Test {
    protected:
+    Subject sub{};
 };
+
+TEST_F(SubjectTest, attachShouldWorkCorrectly) {
+    const auto observer = new MockObserver{};
+    sub.attach(observer);
+
+    const auto justAddedObservers = sub.getObservers();
+    ASSERT_FALSE(justAddedObservers.empty());
+
+    const auto lastAddedObserver = static_cast<MockObserver*>(justAddedObservers.at(0));
+    EXPECT_TRUE(lastAddedObserver != nullptr);
+    EXPECT_TRUE(observer == lastAddedObserver);
+}
+
+TEST_F(SubjectTest, detachShouldWorkCorrectly) {
+    const auto observer = new MockObserver{};
+    sub.attach(observer);
+    sub.detach(observer);
+
+    const auto justAddedObservers = sub.getObservers();
+    EXPECT_TRUE(justAddedObservers.empty());
+}
+
+TEST_F(SubjectTest, notifyShouldWorkCorrectly) {
+    const auto observer = new MockObserver{};
+    sub.attach(observer);
+    sub.notify();
+
+    EXPECT_TRUE(observer->isUpdated());
+}
