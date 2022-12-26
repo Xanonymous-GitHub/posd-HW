ab07ee4e1d041c119e003d324a3092857980aafd
POSD <lab1321@gmail.com>
2022-12-26 14:30:42 +0800

    exam: update ta code

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,81 +1,62 @@
-#include "./point.h"
-#include "./two_dimensional_vector.h"
-
-#include "./shape.h"
-
-#include "./circle.h"
-#include "./rectangle.h"
-#include "./triangle.h"
-
-#include "./compound_shape.h"
+#include <iostream>
 
 #include "./bounding_box.h"
 #include "./builder/shape_parser.h"
+#include "./circle.h"
+#include "./compound_shape.h"
+#include "./graphics/drag_and_drop/command/command_history.h"
+#include "./graphics/drag_and_drop/command/drop_command.h"
+#include "./graphics/drag_and_drop/command/grab_command.h"
+#include "./graphics/drag_and_drop/command/move_command.h"
+#include "./graphics/drag_and_drop/command/undo_command.h"
+#include "./graphics/drag_and_drop/drag_and_drop.h"
+#include "./graphics/drawing.h"
+#include "./graphics/event_listener.h"
+#include "./graphics/real_canvas.h"
 #include "./graphics/sdl/sdl_renderer.h"
 #include "./graphics/sdl_adapter.h"
 #include "./iterator/bfs_compound_iterator.h"
 #include "./iterator/dfs_compound_iterator.h"
 #include "./iterator/list_compound_iterator.h"
 #include "./iterator/null_iterator.h"
+#include "./point.h"
+#include "./rectangle.h"
+#include "./shape.h"
+#include "./triangle.h"
+#include "./two_dimensional_vector.h"
 #include "./utils/file_reader.h"
 #include "./visitor/collision_detector.h"
-#include "./visitor/shape_printer.h"
 
-#include "./graphics/drag_and_drop/command/command_history.h"
-#include "./graphics/drag_and_drop/command/drop_command.h"
-#include "./graphics/drag_and_drop/command/grab_command.h"
-#include "./graphics/drag_and_drop/command/move_command.h"
-#include "./graphics/drag_and_drop/command/refresh_command.h"
-#include "./graphics/drag_and_drop/command/undo_command.h"
-#include "./graphics/drag_and_drop/drag_and_drop.h"
-#include "./graphics/event_listener.h"
-
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-
-#define DEBUGMSG(msg) std::cout << msg << std::endl;
-
-class BadArgumentException : public std::runtime_error {
-    using std::runtime_error::runtime_error;
-};
-
-int main(int argc, const char *args[]) {
+int main(int argc, char *args[]) {
     if (argc < 2) {
         std::cout << "Missing the path of input.txt" << std::endl;
-        throw BadArgumentException{"Missing the path of input.txt"};
+        throw "Missing the path of input.txt";
     }
-
-    const FileReader reader{args[1]};
-    const auto input = reader.read();
-
-    ShapeParser parser{input};
+    FileReader reader(args[1]);
+    std::string input = reader.read();
+    ShapeParser parser(input);
     parser.parse();
+    std::vector<Shape *> shapes = parser.getResult();
 
-    const auto shapes = parser.getResult();
-    const auto eventListener = std::make_unique<EventListener>();
-    const std::unique_ptr<SDL> renderer = std::make_unique<SDLRenderer>(30, eventListener.get());
-    const std::unique_ptr<Canvas> canvas = std::make_unique<SDLAdapter>(1024, 768, renderer.get());
+    EventListener *eventListener = new EventListener();
+    SDL *renderer = new SDLRenderer(30, eventListener);    // Adaptee
+    Canvas *canvas = new SDLAdapter(1024, 768, renderer);  // Adapter
 
-    const auto dragAndDrop = std::make_unique<DragAndDrop>(shapes);
-    const auto commandHistory = std::make_unique<CommandHistory>();
+    Drawing *drawing = new Drawing(shapes);                    // Subject
+    RealCanvas *realCanvas = new RealCanvas(canvas, drawing);  // Observer
 
-    eventListener->on("Left_Mouse_Down", new GrabCommand(dragAndDrop.get(), commandHistory.get()));
-    eventListener->on("Left_Mouse_Move", new MoveCommand(dragAndDrop.get(), commandHistory.get()));
-    eventListener->on("Left_Mouse_Up", new DropCommand(dragAndDrop.get(), commandHistory.get()));
-    eventListener->on("Right_Mouse_Down", new UndoCommand(dragAndDrop.get(), commandHistory.get()));
-    eventListener->on("Refresh", new RefreshCommand(canvas.get(), shapes));
+    DragAndDrop *dragAndDrop = new DragAndDrop(drawing);
+    CommandHistory *commandHistory = new CommandHistory();
+    eventListener->on("Left_Mouse_Down", new GrabCommand(dragAndDrop, commandHistory));
+    eventListener->on("Left_Mouse_Move", new MoveCommand(dragAndDrop, commandHistory));
+    eventListener->on("Left_Mouse_Up", new DropCommand(dragAndDrop, commandHistory));
+    eventListener->on("Right_Mouse_Down", new UndoCommand(dragAndDrop, commandHistory));
+    canvas->display();
 
-    const auto VALGRIND_MODE = std::getenv("VALGRIND_MODE");
-    const auto shouldStartDisplay = VALGRIND_MODE == nullptr || std::string_view(VALGRIND_MODE) != "true";
-
-    if (shouldStartDisplay) {
-        canvas->display();
-    }
-
-    for (auto &&it : shapes) {
-        delete it;
-    }
+    delete eventListener;
+    delete renderer;
+    delete canvas;
+    delete drawing;
+    delete realCanvas;
+    delete dragAndDrop;
 }
--- src/graphics/drag_and_drop/drag_and_drop.h
+++ src/graphics/drag_and_drop/drag_and_drop.h
@@ -1,52 +1,51 @@
 #pragma once
 
+#include <iostream>
+
 #include "../../circle.h"
 #include "../../compound_shape.h"
 #include "../../shape.h"
 #include "../../two_dimensional_vector.h"
 #include "../../visitor/collision_detector.h"
-#include <iostream>
-#include <vector>
+#include "../drawing.h"
 
 class DragAndDrop {
-private:
-    std::vector<Shape *> _shapes;
+   private:
+    Drawing *_drawing;
     Shape *_target_shape = nullptr;
     double _previousX, _previousY;
 
-public:
-    DragAndDrop() = default;
-    DragAndDrop(const std::vector<Shape *> &shapes) : _shapes{shapes} {}
-    ~DragAndDrop() = default;
+   public:
+    DragAndDrop() {}
+    DragAndDrop(Drawing *drawing) : _drawing(drawing) {
+        _drawing->notify();
+    }
+    ~DragAndDrop() {}
 
     virtual void grab(double x, double y) {
-        const Point c1{x, y}, c2{x, y};
-        const TwoDimensionalVector mr{c1, c2};
-        const Circle mousePoint{mr};
-        CollisionDetector cd{&mousePoint};
-
-        for (auto &&it : _shapes) {
-            it->accept(&cd);
-        }
-
-        const auto selectedShapes = cd.collidedShapes();
-
-        if (selectedShapes.size() > 0) {
-            _target_shape = cd.collidedShapes()[0];
-        }
-
+        Point c1(x, y), c2(x, y);
+        TwoDimensionalVector mr(c1, c2);
+        Circle mousePoint(mr);
+        CollisionDetector cd(&mousePoint);
+        std::vector<Shape *> _shapes = _drawing->shapes();
+        for (std::vector<Shape *>::iterator it = _shapes.begin(); it != _shapes.end(); it++)
+            (*it)->accept(&cd);
+        std::vector<Shape *> collidedShapes = cd.collidedShapes();
+        if (collidedShapes.size() != 0)
+            _target_shape = collidedShapes[0];
         _previousX = x;
         _previousY = y;
     }
 
     virtual void move(double x, double y) {
         if (_target_shape != nullptr) {
-            const double deltaX = x - _previousX, deltaY = y - _previousY;
+            double deltaX = x - _previousX, deltaY = y - _previousY;
             _target_shape->move(deltaX, deltaY);
             _previousX = x;
             _previousY = y;
+            _drawing->notify();
         } else {
-            std::cout << "The target shape is not specified yet." << std::endl;
+            throw "The target shape is not specified yet.";
         }
     }
 
